af8df45d91298b144317b841c94df2bc41a0986e
John Munson <19480078+johncmunson@users.noreply.github.com>
2024-02-03 22:34:40 -0600

    add prev/next links to the post page

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { wrap } from '../utils.js'
+import { getNextSlug, getPrevSlug } from '../lib/md.js'
 
 const router = express.Router({ mergeParams: true })
 
@@ -9,8 +10,13 @@ const router = express.Router({ mergeParams: true })
  */
 async function handler(req, res) {
   const postSlug = req.params.postSlug
+  const prevSlug = getPrevSlug(postSlug)
+  const nextSlug = getNextSlug(postSlug)
 
-  res.render('pages/post', { postSlug })
+  const prevLink = prevSlug ? `/posts/${prevSlug}` : null
+  const nextLink = nextSlug ? `/posts/${nextSlug}` : null
+
+  res.render('pages/post', { postSlug, prevLink, nextLink })
 }
 
 router.get('/', wrap(handler))
