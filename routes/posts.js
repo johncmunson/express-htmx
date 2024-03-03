6db879a99a9dde09a547bd5de0c5fb1e9963cfe1
John Munson <19480078+johncmunson@users.noreply.github.com>
2024-03-03 15:47:16 -0600

    finish the tags feature

--- routes/posts.js
+++ routes/posts.js
@@ -10,18 +10,25 @@ const router = express.Router({ mergeParams: true })
  */
 async function handler(req, res) {
   const page = Number(req.query.page) || 1
-  const posts = getPaginatedPosts(page)
+  const tag = /** @type {string | undefined} */ (req.query.tag)
+  const posts = getPaginatedPosts(page, undefined, tag)
 
   if (posts.length === 0) {
     return res.status(404).render('pages/404')
   }
 
-  const prevPosts = page > 1 ? getPaginatedPosts(page - 1) : []
-  const prevLink = prevPosts.length > 0 ? `/posts?page=${page - 1}` : null
-  const nextPosts = getPaginatedPosts(page + 1)
-  const nextLink = nextPosts.length > 0 ? `/posts?page=${page + 1}` : null
+  const prevPosts = page > 1 ? getPaginatedPosts(page - 1, undefined, tag) : []
+  const prevLink =
+    prevPosts.length > 0
+      ? `/posts?page=${page - 1}${tag ? `&tag=${tag}` : ''}`
+      : null
+  const nextPosts = getPaginatedPosts(page + 1, undefined, tag)
+  const nextLink =
+    nextPosts.length > 0
+      ? `/posts?page=${page + 1}${tag ? `&tag=${tag}` : ''}`
+      : null
 
-  res.render('pages/posts', { posts, prevLink, nextLink })
+  res.render('pages/posts', { posts, tag, prevLink, nextLink })
 }
 
 router.get('/', wrap(handler))
