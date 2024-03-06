ec95d2b60624974b9d98cdaef0e80450c6c4ec7c
John Munson <19480078+johncmunson@users.noreply.github.com>
2024-03-06 03:43:21 -0600

    finish the popular posts feature. not super happy with the ugly logic going on in posts.js and the getPaginatedPosts function. but, it works. also, i'd like to change the route from /posts?popular=true to /posts/popular

--- routes/posts.js
+++ routes/posts.js
@@ -11,24 +11,33 @@ const router = express.Router({ mergeParams: true })
 async function handler(req, res) {
   const page = Number(req.query.page) || 1
   const tag = /** @type {string | undefined} */ (req.query.tag)
-  const posts = getPaginatedPosts(page, undefined, tag)
+  const popular = /** @type {Boolean | undefined} */ (
+    Boolean(req.query.popular)
+  )
+
+  const posts = getPaginatedPosts(page, undefined, tag, popular)
 
   if (posts.length === 0) {
     return res.status(404).render('pages/404')
   }
 
-  const prevPosts = page > 1 ? getPaginatedPosts(page - 1, undefined, tag) : []
+  const prevPosts =
+    page > 1 ? getPaginatedPosts(page - 1, undefined, tag, popular) : []
   const prevLink =
     prevPosts.length > 0
-      ? `/posts?page=${page - 1}${tag ? `&tag=${tag}` : ''}`
+      ? `/posts?page=${page - 1}${tag ? `&tag=${tag}` : ''}${
+          popular ? `&popular=true` : ''
+        }`
       : null
-  const nextPosts = getPaginatedPosts(page + 1, undefined, tag)
+  const nextPosts = getPaginatedPosts(page + 1, undefined, tag, popular)
   const nextLink =
     nextPosts.length > 0
-      ? `/posts?page=${page + 1}${tag ? `&tag=${tag}` : ''}`
+      ? `/posts?page=${page + 1}${tag ? `&tag=${tag}` : ''}${
+          popular ? `&popular=true` : ''
+        }`
       : null
 
-  res.render('pages/posts', { posts, tag, prevLink, nextLink })
+  res.render('pages/posts', { posts, tag, popular, prevLink, nextLink })
 }
 
 router.get('/', wrap(handler))
